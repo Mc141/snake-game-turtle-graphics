09f1d4244fa014e0ec8bd8be3936f65e841d56db
Mc141 <marthinuswolmarans61@gmail.com>
2024-04-05 00:11:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, time
 
 screen = Screen()
 
@@ -6,9 +6,15 @@ screen.screensize(600, 600)
 screen.setup(602, 602)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
-sqaures = []
-square_x_posistions = [-40, -20, 0]
+
+def left():
+    squares[0].left(90)
+
+
+squares = []
+square_x_posistions = [0, -20, -40]
 
 for square_index in range(0, 3):
     square = Turtle()
@@ -16,12 +22,38 @@ for square_index in range(0, 3):
     square.shape("square")
     square.penup()
     square.goto(square_x_posistions[square_index], 0)
-    sqaures.append(square)
+    squares.append(square)
+
+
+game_is_running = True
+
+
+while game_is_running:
+    time.sleep(0.1)
+    screen.update()
+
+    for square_index in range(len(squares) - 1, 0, -1):
+        old_x_posistion = squares[square_index - 1].xcor()
+        old_y_posistion = squares[square_index - 1].ycor()
+        squares[square_index].goto(old_x_posistion, old_y_posistion)
+
+    squares[0].forward(20)
+
+    screen.onkey(left, "a")
+    screen.listen()
+
+    
+    
+
+
+
+    
 
 
 
 
 
+        
 
 
 
